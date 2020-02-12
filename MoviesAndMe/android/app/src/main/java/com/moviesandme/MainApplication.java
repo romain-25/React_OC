735919894fc7855bfcf4f528ae86a20e09b91bc2
Romain-25 <romain.rouabah@gmail.com>
2020-02-12 15:36:40 +0100

    android

--- MoviesAndMe/android/app/src/main/java/com/moviesandme/MainApplication.java
+++ MoviesAndMe/android/app/src/main/java/com/moviesandme/MainApplication.java
@@ -3,14 +3,14 @@ package com.moviesandme;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.moviesandme.generated.BasePackageList;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.swmansion.rnscreens.RNScreensPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
--- MoviesAndMe/index.js
+++ MoviesAndMe/index.js
@@ -1,9 +1,5 @@
-import { AppRegistry, Platform } from 'react-native';
+// index.js
+import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('MoviesAndMe', () => App);
-
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('MoviesAndMe', { rootTag });
-}
